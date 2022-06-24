8d4cbb7d57292510e06d3f4fbae24a9e90cff68d
eyi-jin <jmj2588547@naver.com>
2022-06-24 19:37:45 +0900

    2-2. spring web basic: MVC & template engine

--- src/main/java/eyijin/hellospring/HelloController/HelloController.java
+++ src/main/java/eyijin/hellospring/HelloController/HelloController.java
@@ -3,6 +3,8 @@ package eyijin.hellospring.HelloController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,4 +13,11 @@ public class HelloController {
         model.addAttribute("data","hello!" );
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
 }
