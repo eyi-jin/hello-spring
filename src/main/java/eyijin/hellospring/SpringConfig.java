8ab1a454cdeffbcdfec919be9fd59448936498c7
eyi-jin <jmj2588547@naver.com>
2022-06-26 21:46:24 +0900

    6-4. spring DB access: spring JdbcTemplate

--- src/main/java/eyijin/hellospring/SpringConfig.java
+++ src/main/java/eyijin/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package eyijin.hellospring;
 
 import eyijin.hellospring.repository.JdbcMemberRepository;
+import eyijin.hellospring.repository.JdbcTemplateMemberRepository;
 import eyijin.hellospring.repository.MemberRepository;
 import eyijin.hellospring.repository.MemoryMemberRepository;
 import eyijin.hellospring.service.MemberService;
@@ -24,7 +25,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
 //        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
